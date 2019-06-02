69f88c7250764c7e2a92e6495583d92e7d75868f
kyoung hee park <>
2019-06-02 09:24:13 +0900

    DataBinding - Basics

--- app/src/main/java/com/may/myandroidsamples/MainActivity.kt
+++ app/src/main/java/com/may/myandroidsamples/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.may.myandroidsamples
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.may.myandroidsamples.databinding.ActivityMainBinding
+import com.may.myandroidsamples.viewmodel.User
 
 class MainActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+
+        // Using DataBinding
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.user = User("May", "Park")
+
     }
 }
---
+++ app/src/main/java/com/may/myandroidsamples/viewmodel/User.kt
@@ -0,0 +1,4 @@
+package com.may.myandroidsamples.viewmodel
+
+
+data class User(val firstName:String, val lastName:String)

f2bdffba5edd25bf08d41b89767a0d0c975a2617
kyoung hee park <>
2019-06-02 15:28:30 +0900

    Kotlin Basics - Conditional and Functions

--- app/src/main/java/com/may/myandroidsamples/MainActivity.kt
+++ app/src/main/java/com/may/myandroidsamples/MainActivity.kt
@@ -4,6 +4,7 @@ import android.arch.lifecycle.ViewModelProvider
 import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.may.myandroidsamples.databinding.ActivityMainBinding
 import com.may.myandroidsamples.viewmodel.User
 
@@ -20,5 +21,66 @@ class MainActivity : AppCompatActivity() {
 
         // Set MyHandlers
         binding.handlers = MyHandlers()
+
+
+        // Conditional
+        val count = 30
+        val conditionalString = when {
+            count == 30 -> "count is 30"
+            count > 35 -> "count > 35??"
+            else -> "else ~~ "
+        }
+        Log.d("MySample", conditionalString)
+
+
+        // Function call
+        val answerString = generateAnswerString(20)
+        Log.d("MySample", answerString)
+
+        // Anonymous functions
+        val stringLenthFunc : (String) -> Int = {
+            it.length
+        }
+        val stringLenth : Int = stringLenthFunc("Hello~")
+        Log.d("MySample", "stringLenth is $stringLenth")
+
+
+        // Higher-order function call
+        val highOrderFunctionVall : Int = stringMapper("Hi there!", ({
+            it.length + 10
+        }))
+        Log.d("MySample", "Higher-order function call. stringLenth is $highOrderFunctionVall")
+
+
+
     }
+
+    // Kotlin's function
+    fun generateAnswerString(countThreshold: Int) : String {
+        val count = 30
+        // first syntax
+//        val answerString = if (count > countThreshold) {
+//            "I have the answer"
+//        } else {
+//            "The answer eludes me"
+//        }
+//        return answerString
+
+        // value 생략 바로 리턴
+        return if (count > countThreshold) {
+            "I have the answer"
+        } else {
+            "The answer eludes me"
+        }
+    }
+
+    // Higher-order function
+    fun stringMapper(str: String, mapper: (String) -> Int) : Int {
+        // Invoke function
+        return mapper(str)
+    }
+
+
+
+
 }
